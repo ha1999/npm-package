e8022b1ab1e2caac83c4331e470e32edeb093e5d
ha1999 <leha220699@gmail.com>
2021-02-19 21:26:19 +0700

    update to version 1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-export function sayHello(): void {
-    alert("hello world")
-}
+const crypto = require('crypto');
+const { generateKeyPairSync } = require('crypto');
+
+export const generateKey = () => generateKeyPairSync('rsa', {
+  modulusLength: 4096,
+  publicKeyEncoding: {
+    type: 'spki',
+    format: 'pem'
+  },
+  privateKeyEncoding: {
+    type: 'pkcs8',
+    format: 'pem',
+    cipher: 'aes-256-cbc',
+    passphrase: 'top secret'
+  }
+});
+export const encryptedData = (data, publicKey) => crypto.publicEncrypt(
+	{
+		key: publicKey,
+		padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+		oaepHash: "sha256",
+	},
+	Buffer.from(data)
+).toString('base64')
+export const decryptedData = (encryptedData, privateKey) => crypto.privateDecrypt(
+	{
+		key: privateKey,
+		padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+		oaepHash: "sha256",
+		passphrase: 'top secret',
+	},
+	encryptedData
+).toString()
